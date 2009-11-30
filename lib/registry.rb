2ea17bf30b5be1a68b75ef93dc2815b974c710d1
Ben Schwarz <ben.schwarz@gmail.com>
2009-11-30 22:08:36 +1100

    Added implementation and specs to cover

---
+++ lib/registry.rb
@@ -0,0 +1,22 @@
+module Registry
+  @@registered = {}
+  
+  class NotRegistered < StandardError; end
+  
+  def inherited(klass)
+    klass.send(:indentifier, klass.name.downcase.to_sym)
+  end
+  
+  def for(id, &block)
+    if @@registered.has_key? id
+      @@registered[id].class_eval &block
+    else
+      raise NotRegistered, "There is no #{self.name} registered for #{id}"
+    end
+  end
+  
+  private
+  def indentifier(*identifiers)
+    identifiers.each {|i| @@registered[i] = self }
+  end
+end
--- spec/registry_spec.rb
+++ spec/registry_spec.rb
@@ -1,7 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Registry" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+class Parent
+  extend Registry
+end
+
+class Stacey < Parent
+end
+
+class Tracey < Parent
+  indentifier :child, :female
+  
+  def method_call
+  end
+end
+
+
+describe Registry do
+  describe "Parent" do
+    it "should respond to for" do
+      Parent.should respond_to :for
+    end
+  end
+  
+  describe Stacey, Tracey do
+    it "should automatically register the class name" do
+      lambda { Parent.for(:stacey) { true } }.should be_true
+    end
+    
+    it "should be registered to handle child" do
+      Tracey.should_receive(:method_call)
+      Parent.for(:child) { method_call }
+    end
+    
+    it "should be registered to handle child" do
+      Tracey.should_receive(:method_call)
+      Parent.for(:female) { method_call }
+    end
+    
+    it "should raise NotRegistered for unregistered classes" do
+      lambda { Parent.for(:xyz) { method_call } }.should raise_error(Registry::NotRegistered)
+    end
   end
 end
